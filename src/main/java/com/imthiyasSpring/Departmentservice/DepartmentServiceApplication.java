c972ebec4930f8579898304cd108e6740f933cc6
mohamediyas <mohamedimthiyas24@gmail.com>
2024-01-08 15:56:53 +0530

    spring doc added swagger-ui/index.html

--- src/main/java/com/imthiyasSpring/Departmentservice/DepartmentServiceApplication.java
+++ src/main/java/com/imthiyasSpring/Departmentservice/DepartmentServiceApplication.java
@@ -1,9 +1,35 @@
 package com.imthiyasSpring.Departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service Rest Apis",
+				description = "Department Service Rest api documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Mohamed imthiyas",
+						email = "mohamedimthiyas@gmail.com",
+						url = "https://www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "www.google.com"
+				)
+
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department service docs",
+				url = "www.google.com"
+		)
+)
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/imthiyasSpring/Departmentservice/controller/DepartmentController.java
+++ src/main/java/com/imthiyasSpring/Departmentservice/controller/DepartmentController.java
@@ -3,11 +3,18 @@ package com.imthiyasSpring.Departmentservice.controller;
 
 import com.imthiyasSpring.Departmentservice.dto.DepartmentDto;
 import com.imthiyasSpring.Departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Department controller",
+        description = "Department controller expose endpoint for department service"
+)
 @RestController
 @RequestMapping("api/departments")
 @AllArgsConstructor
@@ -17,6 +24,14 @@ public class DepartmentController {
 
 
 //    save department
+    @Operation(
+            summary = "Save Department Rest api",
+            description = "save department to store department data"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http status 201 created"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
         DepartmentDto departmentDto1 = departmentService.saveDepartment(departmentDto);
@@ -26,6 +41,14 @@ public class DepartmentController {
 
 //    get by id
 
+    @Operation(
+            summary = "Get Department Rest api",
+            description = "Get department to view department data"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http status 200 retrived"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
